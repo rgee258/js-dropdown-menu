8d8a0f0d23289ebb7e1c8db5bd1c0dcb3d42ae12
rgee258 <9219093+rgee258@users.noreply.github.com>
2019-09-09 00:09:18 -0400

    Added basic menu structure and js components

---
+++ src/modules/dropdown_menu.js
@@ -0,0 +1,44 @@
+function addToggles() {
+  let labels = document.querySelectorAll('.dd-label');
+  let dropdowns = document.querySelectorAll('.dd-content');
+
+  // Add label click to remove displayed menus and then show the new one
+  labels.forEach(function(label) {
+    label.addEventListener('click', function() {
+      removeMenus();
+      showDropdown(label);
+    });
+  });
+
+  // Remove the dropdown when the mouse leaves its contents
+  dropdowns.forEach(function(dd) {
+    dd.addEventListener('mouseleave', function() {
+      removeMenus();
+    });
+  });
+
+  // If the mouse never enters the content, remove it when we leave the menu
+  document.querySelector('.dropdown-menu')
+    .addEventListener('mouseleave', function() {
+      removeMenus();
+    }
+  );
+
+}
+
+function showDropdown(label) {
+  // Each label list item will have only two children: the span text and
+  // the dropdown list so the last child will always be the dropdown
+  let dropdown = label.lastElementChild;
+  dropdown.classList.add('show-dd');
+}
+
+function removeMenus() {
+  let shown = document.querySelectorAll('.show-dd');
+
+  shown.forEach(function(dd) {
+    dd.classList.remove('show-dd');
+  });
+}
+
+export { addToggles }
