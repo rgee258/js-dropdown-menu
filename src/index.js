1aeacf9217169a481305fa8684b17ab349798813
Russell <rgee258@gmail.com>
2019-09-09 00:09:18 -0400

    Added basic menu structure and js components

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { addToggles } from './modules/dropdown_menu.js';
+import css from './css/style.css';
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    addToggles();
+});
